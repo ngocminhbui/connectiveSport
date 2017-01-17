5b0bdd4e8a9ce48a0bf75443cee44bd9818d63b5
luongquocan196 <lqa.ntk@gmail.com>
2017-01-17 13:41:16 +0700

    k

--- ConnectiveSport/ViewModels/FriendPageViewModel.cs
+++ ConnectiveSport/ViewModels/FriendPageViewModel.cs
@@ -6,6 +6,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System;
 using System.Diagnostics;
+using Facebook;
 
 namespace ConnectiveSport
 {
@@ -20,12 +21,19 @@ namespace ConnectiveSport
 
 		public void GetAvailableFriends(bool forceRefresh = false)
 		{
-			//dummy friends
-			FriendsList = new ObservableCollection<AthleteProfileViewModel>();
-			FriendsList.Add(new AthleteProfileViewModel(new Athelete { Name = "An Luong", Alias = "An cute :v" ,ImageURL = "http://findicons.com/files/icons/1072/face_avatars/300/a04.png" }));
-			FriendsList.Add(new AthleteProfileViewModel(new Athelete { Name = "Le Do", Alias = "Le cute :v", ImageURL = "http://findicons.com/files/icons/1072/face_avatars/300/a04.png" }));
-			FriendsList.Add(new AthleteProfileViewModel(new Athelete { Name = "Minh Bui", Alias = "Minh :v", ImageURL = "http://findicons.com/files/icons/1072/face_avatars/300/a04.png" }));
-		}
+            FacebookClient fb = new FacebookClient(Settings.AccessToken);
+            string Name = "";
+            string Url = "";
+
+            dynamic FriendlistData = fb.Get("me/friends?fields=picture,name");
+            foreach (var friend in FriendlistData["data"])
+            {
+                Name = friend["name"];
+                dynamic picture = friend["picture"];
+                Url = picture["data"]["url"];
+                FriendsList.Add(new AthleteProfileViewModel(new Athelete { Name = Name, Alias = Name + " cute", ImageURL = Url }));
+            }
+        }
 	}
 }
 
